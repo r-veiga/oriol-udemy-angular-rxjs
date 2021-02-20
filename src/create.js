f329cf1db8feb038114d15e3700ff4b87bbbed1a
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-20 09:22:22 +0100

    from() convierte PROMISE en OBSERVABLE

--- src/create.js
+++ src/create.js
@@ -2,14 +2,24 @@ import { displayLogInScreen } from './utils';
 import { from } from 'rxjs';
 
 export default () => {
-
-    const myArray = [1,2,3,4,5]; 
-    const myString = 'Hello world';
-
-    const observable1 = from(myArray);
-    const subscription1 = observable1.subscribe(val => displayLogInScreen(val));
-
-    const observable2 = from(myString);
-    const subscription2 = observable2.subscribe(val => displayLogInScreen(val));
-
+    //🐷🐷 Ejemplo previo
+    //🐷🐷
+    // const myArray = [1,2,3,4,5]; 
+    // const observable1 = from(myArray);
+    // const subscription1 = observable1.subscribe(val => displayLogInScreen(val));
+    //
+    // const myString = 'Hello world';
+    // const observable2 = from(myString);
+    // const subscription2 = observable2.subscribe(val => displayLogInScreen(val));
+    
+    const myPromise = new Promise(resolve => 
+        setTimeout(() => {
+            resolve('Hello World')
+        }
+        , 2 * 1000));
+        
+    //🐷🐷 El uso habitual del operador from() 
+    //🐷🐷 es convertir PROMISE en OBSERVABLE
+    const observable = from(myPromise);
+    const subscription = observable.subscribe(val => displayLogInScreen(val));
 }
