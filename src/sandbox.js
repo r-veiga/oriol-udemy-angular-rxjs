ebe69325668588258b07361ce93b92b1ddd97bbb
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-21 00:27:15 +0100

    "first()" se queda sólo con el 1er valor emitido

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,20 @@
-import { displayLog } from './utils';
+import { displayLog as displayLogInScreen } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
+
+    // 🐷🐷 "first()" cuando sólo quiero el primer elemento emitido, ignora los demás
+
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]), 
+        first() // 🐷🐷
     );
 
-    const subscription = click$.subscribe(data => displayLog(data));
+    const subscription = click$.subscribe(data => displayLogInScreen(data));
 
-    /** end coding */
 }
