16afe892861d17d77b26b278ade2be8727874a42
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-20 08:44:33 +0100

    Cancelar subscripción DENTRO del flujo de eventos

--- src/create.js
+++ src/create.js
@@ -5,9 +5,28 @@ export default () => {
     // 🐷 DEPRECATED 🐷 const hola = Observable.create(function(observer) {
     const hola = new Observable((observer) => {
         observer.next("Hello");
-        setTimeout(() => { observer.next("How you doing?"); }, 2 * 1000);
+
+
+        // 🐷🐷 de la manera original, el mensaje asíncrono "How you doing?" no se muestra
+        // 🐷🐷 porque se ha ejecutado ".complete()" antes, debido al lag de 2 segundos
+        // 🐷🐷 deberé incluir el complete() dentro del "setTimeout"
+        // 🐷🐷 
+        // setTimeout(() => { observer.next("How you doing?"); }, 2 * 1000);
+        // observer.next("world");
+        // observer.complete();
+        // 🐷🐷 
+        setTimeout(() => { 
+            observer.next("How you doing?"); 
+            observer.complete();
+        }, 2 * 1000);
         observer.next("world");
     });
 
-    const subscribe = hola.subscribe(evt => displayLogInScreen(evt));
+    const observer = {
+        next: evt => displayLogInScreen(evt),
+        error: err => console.err("[ERR] - ", err), 
+        complete: () => displayLogInScreen("[DONE]")
+    }
+
+    const subscribe = hola.subscribe(observer);
 }
