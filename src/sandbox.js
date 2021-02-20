c81331e6d8aeb582babe0b8a29d67e19d4575081
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-20 22:26:41 +0100

    operador mapTo() devuelve siempre mismo valor

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,11 +1,18 @@
 import { displayLog } from './utils';
 import { fromEvent } from "rxjs";
+import { mapTo } from "rxjs/operators";
 
 export default () => {
 
+    // 🐷🐷 Los operadores, dentro de "pipe"
+    // 🐷🐷 "mapTo()" devuelve siempre el mismo valor cualquiera que sea la entrada
+    
     const grid = document.getElementById('grid');
     
-    const click$ = fromEvent(grid, 'click');
+    const click$ = fromEvent(grid, 'click').pipe(
+        mapTo('mouse is CLICKED')  // 🐷🐷
+    );
     const subscription = click$.subscribe(data => console.log(data));
-    
+
+
 }
