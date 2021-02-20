a0d3da86e83ababfba97a7bd4b888449b937e7ca
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-20 22:15:11 +0100

    fromEvent() sobre movimiento del ratón

--- src/create.js
+++ src/create.js
@@ -6,10 +6,16 @@ export default () => {
     // 🐷🐷 fromEvent() crea un Observable que emite eventos 
     // 🐷🐷 de un tipo específico provinientes de un target event dado 
     
+    // (1) sobre evento CLICK de botón
     const actionBtn = document.getElementById('action-btn');
     const source = fromEvent(actionBtn, 'click');
 
     source.subscribe(event => {
         displayLogInScreen(`click event en la posición (${event.x}, ${event.y})`)
+    })
+
+    // (2) sobre evento MOUSEMOVE del ratón
+    fromEvent(document, 'mousemove').subscribe(evt => {
+        console.log(evt)
     })
 }
