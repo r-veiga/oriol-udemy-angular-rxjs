81584c2e8bfc2e0e6cde4036e0d285081a53a328
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-21 00:33:19 +0100

    "take( N )" quiero los N primeros items emitidos

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,11 +1,12 @@
 import { displayLog as displayLogInScreen } from './utils';
 import { fromEvent } from 'rxjs';
-import { first, map } from 'rxjs/operators';
+import { first, map, take } from 'rxjs/operators';
 
 export default () => {
 
     // 🐷🐷 "first()" cuando sólo quiero el primer elemento emitido, ignora los demás
     // 🐷🐷           se le puede aplicar un predicado (1er valor que cumpla predicado)
+    // 🐷🐷 "take( N )" quiero los primeros N elementos emitidos
 
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
@@ -13,7 +14,7 @@ export default () => {
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
         ]), 
-        first(val => val[0] > 3) // 🐷🐷 sólo primer click en la 4ª columna o superior
+        take(4) // 🐷🐷 cuatro primeros eventos, cierra el stream después
     );
 
     const subscription = click$.subscribe(data => displayLogInScreen(data));
