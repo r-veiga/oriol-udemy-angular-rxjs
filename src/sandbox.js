a97ee27029403b7644f3279fc29ebb9ec10e3b4d
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-21 00:03:45 +0100

    Operador tap() para trazas de log en el pipe()

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,23 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
+
+    // 🐷🐷 el operador "tap()" crea efectos colaterales en el Observable fuente, 
+    // 🐷🐷 devolviendo un Observable idéntico a la fuente
+    // 🐷🐷 Cuando quieres modificar algo que no tiene que ver con el flujo de datos (aunque dependa de ello)
+
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
+        tap(val => console.log('before: ', val)),
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        tap(val => console.log(`after: , ${val}`))
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
 
-    /** end coding */
 }
