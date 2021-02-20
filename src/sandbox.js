3e4ed139706c3728b3ab897ae7282bf98eeebf18
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-20 22:33:58 +0100

    map() aplica función sobre cada valor emitido

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,16 +1,17 @@
 import { displayLog } from './utils';
 import { fromEvent } from "rxjs";
-import { mapTo } from "rxjs/operators";
+import { map } from "rxjs/operators";
 
 export default () => {
 
     // 🐷🐷 Los operadores, dentro de "pipe"
     // 🐷🐷 "mapTo()" devuelve siempre el mismo valor cualquiera que sea la entrada
+    // 🐷🐷 "map()" aplica una función a cada valor emitido, en este caso crea un array de posición [x,y]
     
     const grid = document.getElementById('grid');
     
     const click$ = fromEvent(grid, 'click').pipe(
-        mapTo('mouse is CLICKED')  // 🐷🐷
+        map(val => [val.offsetX, val.offsetY]) // 🐷🐷
     );
     const subscription = click$.subscribe(data => console.log(data));
 
