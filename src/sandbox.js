f415f024b7ec79621e22ef97453fe73784725d38
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-21 03:32:58 +0100

    "distinct()" problemático con objectos

--- src/sandbox.js
+++ src/sandbox.js
@@ -5,6 +5,8 @@ import { distinct, map, takeWhile, tap } from 'rxjs/operators';
 export default () => {
 
     // 🐷🐷 "distinct()" sólo deja pasar ítems diferentes a los ya emitidos en el pasado
+    // 🐷🐷              Ojo con los objetos, si sus direcciones de memoria son diferentes (da igual que todas sus propiedades sean iguales)
+    // 🐷🐷              tratar con tipos simples como números es mucho más sencillo
 
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
@@ -13,10 +15,8 @@ export default () => {
             Math.floor(val.offsetY/50)
         ]),
         takeWhile( ([col, row]) => col != 0 ),
-        tap(val => console.log(`cell: [${val}]`)),
-        map(([col, row]) => col+row),
-        tap(val => console.log('sum of col + row is:', val)),
-        distinct()  // 🐷🐷 sólo distintos para un valor numérico simple de suma de fila + columna   
+        tap(val => console.log(`celda: [${val}]`)),
+        distinct()  // 🐷🐷 no se repiten, los objetos creados se consideran distintos   
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
