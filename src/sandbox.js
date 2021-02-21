efc453e378a276329716907d3cf64148e99d9aec
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-02-21 10:26:01 +0100

    Cold Observable
    Hay dos instancias del flujo de datos, lo constato
    al ver que se me duplican las trazas de log.

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,25 +1,36 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, share, tap } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
-    
+
+    // 🐷🐷 Barra de progreso según se avanza scroll en el documento:
+    // 🐷🐷   (1) observable de evento scroll "fromEvent(document, 'scroll')" 
+    // 🐷🐷   (2) observable "scroll$" que me da la posición en ¿píxeles?
+    // 🐷🐷       a partir de un observable de evento de scroll "fromEvent(...)"
+    // 🐷🐷   (3) observable "scrollProgress$" que me da el nº de páginas scrolleadas
+    // 🐷🐷       a partir del observable "scroll$" de posición en píxeles
+    // 🐷🐷 
+    // 🐷🐷 Quiero que la posición del scroll se refleje en los elementos del UI:  
+    // 🐷🐷   (a) barra de progreso
+    // 🐷🐷   (b) texto que muestre el porcentaje en formato "nn%"
+    // 🐷🐷 
+
     const progressBar = document.getElementById('progress-bar');
     const docElement = document.documentElement;
 
-    //function to update progress bar width on view
-    const updateProgressBar = (percentage) => {
-        progressBar.style.width = `${percentage}%`;
-    }
+    // Función Observer para actualizar la anchura de la barra de progreso de la vista/html
+    const updateProgressBar = (percentage) => { progressBar.style.width = `${percentage}%`; }
+    // 🐷🐷 Función Observer para actualizar el texto de % de progreso de la vista/html
+    const updatePercentageText = (percentage) => { updateDisplay(`${ Math.floor(percentage) } %`); } // 🐷🐷
 
-    //observable that returns scroll (from top) on scroll events
+    // Observable que devuelve el posicionamiento de scroll (from top) en eventos de scroll
     const scroll$ = fromEvent(document, 'scroll').pipe(
         map(() => docElement.scrollTop),
         tap(evt => console.log("[scroll]: ", evt))
     );
 
-    //observable that returns the amount of page scroll progress
+    // Observable que a partir de la posición me da el nº de páginas que se ha hecho scroll
     const scrollProgress$ = scroll$.pipe(
         map(evt => {
             const docHeight = docElement.scrollHeight - docElement.clientHeight;
@@ -27,9 +38,10 @@ export default () => {
         })
     )
 
-    //subscribe to scroll progress to paint a progress bar
-    const subscription = scrollProgress$.subscribe(updateProgressBar);
+    // 🐷🐷 Dos suscripciones DISTINTAS al mismo Observable (⚠️ Cold Observable ⚠️):
+    // 🐷🐷   (1) suscripción a scrollProgress$ para pintar una barra de progreso
+    // 🐷🐷   (2) suscripción a scrollProgress$ para escribir el porcentaje por pantalla
+    const subscription1 = scrollProgress$.subscribe(updateProgressBar);
+    const subscription2 = scrollProgress$.subscribe(updatePercentageText);  // 🐷🐷
 
-
-    /** end coding */
 }
